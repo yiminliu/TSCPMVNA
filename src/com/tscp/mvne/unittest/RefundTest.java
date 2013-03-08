44992ac86be9f8d2e64d3b6f3bfe285d550b8714
yiminliu <yimin_l@hotmail.com>
2013-03-08 13:37:13 -0800

    created jUnit test class

---
+++ src/com/tscp/mvne/unittest/RefundTest.java
@@ -0,0 +1,50 @@
+package com.tscp.mvne.unittest;
+
+import static org.junit.Assert.*;
+
+import org.hibernate.classic.Session;
+import org.hibernate.Transaction;
+import org.junit.Before;
+import org.junit.BeforeClass;
+import org.junit.Test;
+
+import com.tscp.mvne.hibernate.HibernateUtil;
+import com.tscp.mvne.refund.RefundDao;
+
+public class RefundTest {
+
+	private RefundDao dao = null;
+	
+	@BeforeClass
+	public static void setUpBeforeClass() throws Exception {
+	}
+
+	@Before
+	public void setUp() throws Exception {
+		dao = new RefundDao();
+	}
+	
+
+	//@Test
+	public void testRefundPayment() {
+	   try{
+	       dao.refundPayment(757617, 6875300, 5723214, "10", "wotg", 1, "test sp");			     
+	   }
+	   catch(Exception e){
+		  e.printStackTrace();
+	   }
+	}
+
+	@Test
+	public void testIsRefunded() {
+		assertTrue(dao.isRefunded(687535));
+		assertTrue(dao.isRefunded(11111111));
+	}
+
+	@Test
+	public void testGetRefundByTransId() {
+		Session session = HibernateUtil.getSessionFactory().getCurrentSession();
+		assertNotNull(dao.getRefundByTransId(session, 757617));
+	}
+
+}
