ea3c22e373201104e07d0d96c2fe5f748f8f8e15
yiminliu <yimin_l@hotmail.com>
2013-03-08 13:39:55 -0800

    clanup code

--- src/com/tscp/mvne/refund/Refund.java
+++ src/com/tscp/mvne/refund/Refund.java
@@ -2,11 +2,6 @@ package com.tscp.mvne.refund;
 
 import java.util.Date;
 
-import javax.persistence.Column;
-import javax.persistence.Entity;
-import javax.persistence.Id;
-import javax.persistence.Table;
-
 
 //@Entity
 //@Table(name="Refund")
